15ff5ab576e1061db0f8094b599e8b1789d342c3
Beat Bolli <bbolli@ewanet.ch>
2011-03-01 17:06:49 +0100

    Expand tabs

--- tumble.py
+++ tumble.py
@@ -16,62 +16,62 @@ DEBUG = False
 def tumble(feed):
     auth = netrc.netrc().authenticators(HOST)
     if auth is not None:
-	auth = {'email': auth[0], 'password': auth[2]}
-	feed = feedparser.parse(feed)
-	return [post(auth, e) for e in feed.entries]
+        auth = {'email': auth[0], 'password': auth[2]}
+        feed = feedparser.parse(feed)
+        return [post(auth, e) for e in feed.entries]
 
 def post(auth, entry):
     enc = entry.get('enclosures', [None])[0]
     if enc and enc.type.startswith('image/'):
-	data = {
-	    'type': 'photo', 'source': enc.href,
-	    'caption': entry.title, 'click-through-url': entry.link
-	}
+        data = {
+            'type': 'photo', 'source': enc.href,
+            'caption': entry.title, 'click-through-url': entry.link
+        }
     elif enc and enc.type.startswith('audio/'):
-	data = {
-	    'type': 'audio', 'caption': entry.title, 'externally-hosted-url': enc.href
-	}
+        data = {
+            'type': 'audio', 'caption': entry.title, 'externally-hosted-url': enc.href
+        }
     elif enc and enc.type.startswith('video/'):
-	data = {
-	    'type': 'video', 'caption': entry.title, 'embed': enc.href
-	}
+        data = {
+            'type': 'video', 'caption': entry.title, 'embed': enc.href
+        }
     elif 'link' in entry:
-	data = {'type': 'link', 'url': entry.link, 'name': entry.title}
-	if 'content' in entry:
-	    data['description'] = entry.content[0].value
-	elif 'summary' in entry:
-	    data['description'] = entry.summary
+        data = {'type': 'link', 'url': entry.link, 'name': entry.title}
+        if 'content' in entry:
+            data['description'] = entry.content[0].value
+        elif 'summary' in entry:
+            data['description'] = entry.summary
     elif 'content' in entry:
-	data = {'type': 'regular', 'title': entry.title, 'body': entry.content[0].value}
+        data = {'type': 'regular', 'title': entry.title, 'body': entry.content[0].value}
     elif 'summary' in entry:
-	data = {'type': 'regular', 'title': entry.title, 'body': entry.summary}
+        data = {'type': 'regular', 'title': entry.title, 'body': entry.summary}
     else:
-	return 'unknown', entry
+        return 'unknown', entry
     if 'tags' in entry:
-	data['tags'] = ','.join('"%s"' % t.term for t in entry.tags)
+        data['tags'] = ','.join('"%s"' % t.term for t in entry.tags)
     for d in ('published_parsed', 'updated_parsed'):
-	if d in entry:
-	    pub = datetime.fromtimestamp(timegm(entry.get(d)))
-	    data['date'] = pub.isoformat(' ')
-	    break
+        if d in entry:
+            pub = datetime.fromtimestamp(timegm(entry.get(d)))
+            data['date'] = pub.isoformat(' ')
+            break
     if BLOG:
-	data['group'] = BLOG
+        data['group'] = BLOG
 
     if DEBUG:
-	return 'debug', entry.id, data
+        return 'debug', entry.id, data
 
     data.update(auth)
     for k in data:
-	if type(data[k]) is unicode:
-	    data[k] = data[k].encode('utf-8')
+        if type(data[k]) is unicode:
+            data[k] = data[k].encode('utf-8')
 
     try:
         return 'ok', urllib2.urlopen('http://' + HOST + '/api/write', urllib.urlencode(data)).read()
     except Exception, e:
-	return 'error', e
+        return 'error', e
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == '-d':
-	DEBUG = True
+        DEBUG = True
     import pprint
     pprint.pprint(tumble(sys.stdin))
