f262ec15592ece28dadf77f4b1d10ed23414d2af
Beat Bolli <bbolli@ewanet.ch>
2012-01-09 22:42:36 +0100

    Reindent

--- tumblr_backup.py
+++ tumblr_backup.py
@@ -35,176 +35,176 @@ def log(s):
 
 class TumblrBackup:
 
-  def get_content(self, post):
-    """generates HTML source for one post"""
+    def get_content(self, post):
+        """generates HTML source for one post"""
 
-    content = []
-    append = content.append
-    type = post('type')
+        content = []
+        append = content.append
+        type = post('type')
 
-    # header info which is the same for all posts
-    append(u'<p class=date>%s</p>\n<!-- type: %s -->' % (post('date'), type))
+        # header info which is the same for all posts
+        append(u'<p class=date>%s</p>\n<!-- type: %s -->' % (post('date'), type))
 
-    if type == 'regular':
-        try:
-            append('<h2>' + unicode(post['regular-title']) + '</h2>')
-        except KeyError:
-            pass
-        try:
-            append(unicode(post['regular-body']))
-        except KeyError:
-            pass
+        if type == 'regular':
+            try:
+                append('<h2>' + unicode(post['regular-title']) + '</h2>')
+            except KeyError:
+                pass
+            try:
+                append(unicode(post['regular-body']))
+            except KeyError:
+                pass
 
-    elif type == 'photo':
-        try:
-            append(unicode(post['photo-caption']))
-        except KeyError:
-            pass
-        append(u'<img alt="" src="%s/%s">' % (self.image_dir, self.save_image(unicode(post['photo-url']))))
+        elif type == 'photo':
+            try:
+                append(unicode(post['photo-caption']))
+            except KeyError:
+                pass
+            append(u'<img alt="" src="%s/%s">' % (self.image_dir, self.save_image(unicode(post['photo-url']))))
 
-    elif type == 'link':
-        text = post['link-text']
-        url = post['link-url']
-        append(u'<h2><a href="%s">%s</a></h2>' % (url, text))
-        try:
-            append(unicode(post['link-description']))
-        except KeyError:
-            pass
+        elif type == 'link':
+            text = post['link-text']
+            url = post['link-url']
+            append(u'<h2><a href="%s">%s</a></h2>' % (url, text))
+            try:
+                append(unicode(post['link-description']))
+            except KeyError:
+                pass
 
-    elif type == 'quote':
-        quote = unicode(post['quote-text'])
-        source = unicode(post['quote-source'])
-        append(u'<blockquote>%s</blockquote>\n<p>%s</p>' % (quote, source))
+        elif type == 'quote':
+            quote = unicode(post['quote-text'])
+            source = unicode(post['quote-source'])
+            append(u'<blockquote>%s</blockquote>\n<p>%s</p>' % (quote, source))
 
-    elif type == 'video':
-        caption = unicode(post['video-caption'])
-        source = unicode(post['video-source'])
-        if source.startswith('<iframe'):
-            append(u'%s\n%s' % (source, caption))
+        elif type == 'video':
+            caption = unicode(post['video-caption'])
+            source = unicode(post['video-source'])
+            if source.startswith('<iframe'):
+                append(u'%s\n%s' % (source, caption))
+            else:
+                player = unicode(post['video-player'])
+                append(u'%s\n%s\n<p><a href="%s">Original</a></p>' % (player, caption, source))
+
+        elif type in ('answer',):
+            return ''
+
         else:
-            player = unicode(post['video-player'])
-            append(u'%s\n%s\n<p><a href="%s">Original</a></p>' % (player, caption, source))
+            append(u'<pre>%s</pre>' % pprint.pformat(post()))
 
-    elif type in ('answer',):
-        return ''
+        tags = post['tag':]
+        if tags:
+            append(u'<p class=tags>%s</p>' % ' '.join('#' + unicode(t) for t in tags))
 
-    else:
-        append(u'<pre>%s</pre>' % pprint.pformat(post()))
+        return '\n'.join(content)
 
-    tags = post['tag':]
-    if tags:
-        append(u'<p class=tags>%s</p>' % ' '.join('#' + unicode(t) for t in tags))
+    def save_post(self, post, content):
+        """saves one post locally"""
 
-    return '\n'.join(content)
+        file_name = os.path.join(self.save_folder, post('id') + '.html')
+        f = codecs.open(file_name, 'w', 'utf-8')
+        try:
+            f.write(self.header + content + self.footer)
+        finally:
+            f.close()
+        self.set_date(file_name)
 
-  def save_post(self, post, content):
-    """saves one post locally"""
-
-    file_name = os.path.join(self.save_folder, post('id') + '.html')
-    f = codecs.open(file_name, 'w', 'utf-8')
-    try:
-        f.write(self.header + content + self.footer)
-    finally:
-        f.close()
-    self.set_date(file_name)
-
-  def save_image(self, image_url):
-    """saves an image if not saved yet"""
-    image_filename = image_url.split('/')[-1]
-    if '.' not in image_filename:
-        image_response = urllib2.urlopen(image_url)
-        image_header = image_response.read(32)
-        image_type = imghdr.what(None, image_header)
-        if image_type:
-            image_type = {'jpeg': 'jpg'}.get(image_type, image_type)
-            image_filename += '.' + image_type
-    else:
-        image_response = None
-        image_header = ''
-    if not os.path.exists(self.image_folder):
-        os.mkdir(self.image_folder)
-    local_image_path = os.path.join(self.image_folder, image_filename)
-    if not os.path.exists(local_image_path):
-        # only download images if they don't already exist
-        if not image_response:
+    def save_image(self, image_url):
+        """saves an image if not saved yet"""
+        image_filename = image_url.split('/')[-1]
+        if '.' not in image_filename:
             image_response = urllib2.urlopen(image_url)
-        image_file = open(local_image_path, 'wb')
-        image_file.write(image_header + image_response.read())
-        image_file.close()
-        self.set_date(local_image_path)
-    if image_response:
-        image_response.close()
-    return image_filename
+            image_header = image_response.read(32)
+            image_type = imghdr.what(None, image_header)
+            if image_type:
+                image_type = {'jpeg': 'jpg'}.get(image_type, image_type)
+                image_filename += '.' + image_type
+        else:
+            image_response = None
+            image_header = ''
+        if not os.path.exists(self.image_folder):
+            os.mkdir(self.image_folder)
+        local_image_path = os.path.join(self.image_folder, image_filename)
+        if not os.path.exists(local_image_path):
+            # only download images if they don't already exist
+            if not image_response:
+                image_response = urllib2.urlopen(image_url)
+            image_file = open(local_image_path, 'wb')
+            image_file.write(image_header + image_response.read())
+            image_file.close()
+            self.set_date(local_image_path)
+        if image_response:
+            image_response.close()
+        return image_filename
 
-  def set_date(self, file_name):
-    os.utime(file_name, (self.post_date, self.post_date))
+    def set_date(self, file_name):
+        os.utime(file_name, (self.post_date, self.post_date))
 
-  def backup(self, account):
-    """makes HTML files for every post on a public Tumblr blog account"""
+    def backup(self, account):
+        """makes HTML files for every post on a public Tumblr blog account"""
 
-    log("Getting basic information\r")
-    base = 'http://' + account + TUMBLR_URL
+        log("Getting basic information\r")
+        base = 'http://' + account + TUMBLR_URL
 
-    # make sure there's a folder to save in
-    self.save_folder = os.path.join(os.getcwd(), account)
-    if not os.path.exists(self.save_folder):
-        os.mkdir(self.save_folder)
-    self.image_dir = 'images'
-    self.image_folder = os.path.join(self.save_folder, self.image_dir)
+        # make sure there's a folder to save in
+        self.save_folder = os.path.join(os.getcwd(), account)
+        if not os.path.exists(self.save_folder):
+            os.mkdir(self.save_folder)
+        self.image_dir = 'images'
+        self.image_folder = os.path.join(self.save_folder, self.image_dir)
 
-    # start by calling the API with just a single post
-    try:
-        response = urllib2.urlopen(base + '?num=1')
-    except urllib2.URLError:
-        sys.stderr.write("Invalid URL %s\n" % base)
-        sys.exit(2)
-    soup = xmltramp.parse(response.read())
+        # start by calling the API with just a single post
+        try:
+            response = urllib2.urlopen(base + '?num=1')
+        except urllib2.URLError:
+            sys.stderr.write("Invalid URL %s\n" % base)
+            sys.exit(2)
+        soup = xmltramp.parse(response.read())
 
-    # collect all the meta information
-    tumblelog = soup.tumblelog
-    title = escape(tumblelog('title'))
-    subtitle = escape(unicode(tumblelog))
+        # collect all the meta information
+        tumblelog = soup.tumblelog
+        title = escape(tumblelog('title'))
+        subtitle = escape(unicode(tumblelog))
 
-    # use it to create a generic header for all posts
-    self.header = u'''<!DOCTYPE html>
+        # use it to create a generic header for all posts
+        self.header = u'''<!DOCTYPE html>
 <html>
 <head><meta charset=utf-8><title>%s</title></head>
 <body>
 <h1>%s</h1>
 ''' % (title, title)
-    if subtitle:
-        self.header += u'<p class=subtitle>%s</p>\n' % subtitle
+        if subtitle:
+            self.header += u'<p class=subtitle>%s</p>\n' % subtitle
 
-    self.footer = u'''
+        self.footer = u'''
 </body>
 </html>
 '''
 
-    # then find the total number of posts
-    total_posts = count or int(soup.posts('total'))
+        # then find the total number of posts
+        total_posts = count or int(soup.posts('total'))
 
-    # then get the XML entries from the API, which we can only do for max 50 posts at once
-    max = 50
-    for i in range(start, start + total_posts, max):
-        # find the upper bound
-        j = i + max
-        if j > start + total_posts:
-            j = start + total_posts
-        log("Getting posts %d to %d of %d...\r" % (i, j - 1, total_posts))
+        # then get the XML entries from the API, which we can only do for max 50 posts at once
+        max = 50
+        for i in range(start, start + total_posts, max):
+            # find the upper bound
+            j = i + max
+            if j > start + total_posts:
+                j = start + total_posts
+            log("Getting posts %d to %d of %d...\r" % (i, j - 1, total_posts))
 
-        response = urllib2.urlopen('%s?num=%d&start=%d' % (base, j - i, i))
-        soup = xmltramp.parse(response.read())
+            response = urllib2.urlopen('%s?num=%d&start=%d' % (base, j - i, i))
+            soup = xmltramp.parse(response.read())
 
-        for post in soup.posts['post':]:
-            try:
-                self.post_date = int(post('unix-timestamp'))
-                content = self.get_content(post)
-                if content:
-                    self.save_post(post, content)
-            except Exception, e:
-                sys.stderr.write('%s%s\n' % (e, 50 * ' '))
+            for post in soup.posts['post':]:
+                try:
+                    self.post_date = int(post('unix-timestamp'))
+                    content = self.get_content(post)
+                    if content:
+                        self.save_post(post, content)
+                except Exception, e:
+                    sys.stderr.write('%s%s\n' % (e, 50 * ' '))
 
-    log("Backup complete" + 50 * ' ' + '\n')
+        log("Backup complete" + 50 * ' ' + '\n')
 
 if __name__ == '__main__':
     import getopt
