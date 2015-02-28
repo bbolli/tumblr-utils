d36f6ad4314ff9733aad06f449a8da277d6a05ad
Beat Bolli <dev@drbeat.li>
2015-02-28 22:39:08 +0100

    tumblr_backup: protect against malformed URLs in get_inline_image()
    Examples from the GitHub issue include URLs starting with "www." instead
    of "http://" and an URL that was just the domain name without a path to
    a image.
    
    Sorry, no saving of images on FTP servers :-)
    
    Fixes #48

--- tumblr_backup.py
+++ tumblr_backup.py
@@ -659,6 +659,8 @@ class TumblrPost:
         if image_url.startswith('//'):
             image_url = 'http:' + image_url
         image_filename = image_url.split('/')[-1]
+        if not image_filename or not image_url.startswith('http'):
+            return match.group(0)
 
         saved_name = self.download_image(image_url, image_filename)
         if saved_name is None:
