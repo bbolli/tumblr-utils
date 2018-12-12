cc13088c51b7ab979fb7d68d35be6410643fe78a
Preston Maness <aggroskater@gmail.com>
2018-12-11 18:08:22 -0600

    Ensure incremental backup of likes works when `--dirs` option in use

--- tumblr_backup.py
+++ tumblr_backup.py
@@ -564,7 +564,8 @@ class TumblrBackup:
                     #log(account, "Finding latest like (may take a while)")
                     ident_max = 0
                     expr = re.compile("(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9]+)?(Z)?")
-                    for f in glob(path_to(post_dir, '*' + post_ext)):
+                    globslug = join('*', dir_index) if options.dirs else '*' + post_ext
+                    for f in glob(path_to(post_dir, globslug)):
                         fh = open(f,'r')
                         for line in fh:
                             res = expr.findall(line)
