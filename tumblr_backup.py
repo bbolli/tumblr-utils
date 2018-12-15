85b4ce3dafd2ff2916f713b41e7d22a363990c12
Beat Bolli <dev@drbeat.li>
2018-12-15 14:41:23 +0100

    tumblr_backup: fix operator precedence
    Dot binds stronger than %.

--- tumblr_backup.py
+++ tumblr_backup.py
@@ -873,9 +873,9 @@ class TumblrPost:
             return match.group(0)
         # get rid of autoplay and muted attributes to align with normal video
         # download behaviour
-        return u'%s%s/%s%s' % (match.group(1), self.media_url,
+        return (u'%s%s/%s%s' % (match.group(1), self.media_url,
             saved_name, match.group(3)
-        ).replace('autoplay="autoplay"', '').replace('muted="muted"', '')
+        )).replace('autoplay="autoplay"', '').replace('muted="muted"', '')
 
     def get_inline_video(self, match):
         """Saves an inline video if not saved yet. Returns the new <video> tag
